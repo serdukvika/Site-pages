36c6e7a53ec0cb4adf3ddd0c11c047e24b4346e7
serdukvika <73019070+serdukvika@users.noreply.github.com>
2021-05-20 11:12:44 +0300

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,11 @@
+$(document).ready(function myFunction(){   
+    $("#button").click(function myFunction(){
+       $("#section").load("header.html");
+    })
+ });
+
+ $(document).ready(function myFunction(){   
+    $("#but").click(function myFunction(){
+       $("#footer").load("footer.html");
+    })
+ });
